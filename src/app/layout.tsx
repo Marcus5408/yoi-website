8ca9f25d5d584cfcb7492a4ccf7a6834caead129
Issac Liu <issac@matchaflavored.site>
2024-04-17 15:49:15 -0400

    fix favicon icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,16 +18,16 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="./favicon.ico" sizes="any" />
         <link
           rel="icon"
-          href="/icon?<generated>"
+          href="./icon?<generated>"
           type="image/<generated>"
           sizes="<generated>"
         />
         <link
           rel="apple-touch-icon"
-          href="/apple-icon?<generated>"
+          href="./apple-icon?<generated>"
           type="image/<generated>"
           sizes="<generated>"
         />
