70267908db9c9d134a83acbbbe819eaa491d59a7
Issac Liu <issac@matchaflavored.site>
2024-04-25 01:52:40 -0400

    Update banner content in banner-mobile.tsx and banner-desktop.tsx

--- src/components/banners/banner-desktop.tsx
+++ src/components/banners/banner-desktop.tsx
@@ -15,7 +15,7 @@ export default function BannerDesktop() {
             src="/wexor-tmg-L-2p8fapOA8-unsplash.jpg"
             width="1080"
           />
-          <div className="flex flex-col justify-center space-y-4 absolute inset-0 w-1/2 sm:2/3 pl-8 pr-8 bg-gradient-to-r from-yoi-blue-4 dark:from-yoi-blue-1 from-40% to-transparent">
+          <div className="flex flex-col justify-center space-y-4 absolute inset-0 w-1/2 sm:2/3 pl-[5%] pr-8 bg-gradient-to-r from-yoi-blue-4 dark:from-yoi-blue-1 from-40% to-transparent">
             <div className="space-y-2 w-2/3 drop-shadow-xl">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none text-yoi-white">
                 Learn.<br />
