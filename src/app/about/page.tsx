97393df06d7830b4e8853a75f8331f562cf42538
Marcus Jones <marcusjones8119@gmail.com>
2024-06-30 02:37:22 -0400

    refactor: Improve automatic redirect on about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,25 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
 export default function RedirectSite() {
-  redirect("/about/team");
-  return (
-    <main className="w-screen h-screen flex flex-col items-center text-center justify-center bg-yoi-white dark:bg-yoi-black text-yoi-black dark:text-yoi-white">
-      <h1 className="text-4xl">Automatic Redirect</h1>
-      <p>
-        Redirecting you to <a href="/about/team" className="text-yoi-blue-1 dark:text-yoi-blue-5">theyoi.org/about/team</a>...
-      </p>
-      <p>If you are not automatically redirected, use the link above.</p>
-    </main>
-  );
+  try {
+    redirect("/about/team");
+  } catch (error) {
+    console.error(error);
+    return (
+      <main className="w-screen h-screen flex flex-col items-center text-center justify-center bg-yoi-white dark:bg-yoi-black text-yoi-black dark:text-yoi-white">
+        <h1 className="text-4xl">Automatic Redirect</h1>
+        <p>
+          Redirecting you to{" "}
+          <a
+            href="/about/team"
+            className="text-yoi-blue-1 dark:text-yoi-blue-5"
+          >
+            theyoi.org/about/team
+          </a>
+          ...
+        </p>
+        <p>If you are not automatically redirected, use the link above.</p>
+      </main>
+    );
+  }
 }
