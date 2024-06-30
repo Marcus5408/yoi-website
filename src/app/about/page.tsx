0f94bba4cc5dfcbb7355dd672e115aad98da4860
Marcus Jones <marcusjones8119@gmail.com>
2024-06-30 02:31:56 -0400

    feat: Add about page with automatic redirect functionality

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function RedirectSite() {
+  return (
+    <main className="w-screen h-screen flex flex-col items-center text-center justify-center bg-yoi-white dark:bg-yoi-black text-yoi-black dark:text-yoi-white">
+      <h1 className="text-4xl">Automatic Redirect</h1>
+      <p>
+        Redirecting you to <a href="about/team" className="text-yoi-blue-1 dark:text-yoi-blue-5">theyoi.org/about/team</a>...
+      </p>
+      <p>If you are not automatically redirected, use the link above.</p>
+    </main>
+  );
+}
