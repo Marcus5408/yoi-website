90e27df3146a05651669ca93e3c79b6e128c8c22
Issac Liu <issac@matchaflavored.site>
2024-07-14 16:55:32 -0400

    chore: Update mapbox configuration and add interactive map

--- src/app/join/chapters/page.tsx
+++ src/app/join/chapters/page.tsx
@@ -1,32 +1,72 @@
 "use client";
 
-import YOINav from "@/components/navigation/navigation.tsx";
-import Banner from "@/components/banners/banner";
-import TextSection from "@/components/sections/text-section";
+import React, { useEffect, useState } from "react";
 import YOIFooter from "@/components/footer";
+import YOINav from "@/components/navigation/navigation";
+import Banner from "@/components/banners/banner";
+import mapboxgl from "mapbox-gl";
+import Head from "next/head";
+import { useMediaQuery } from "react-responsive";
 
 export default function Home() {
+  const [pageIsMounted, setPageIsMounted] = useState(false);
+  /* get size of the device (mobile or desktop) */
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const zoom = isTabletOrMobile ? 2.25 : 4;
+
+  useEffect(() => {
+    setPageIsMounted(true);
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "https://api.mapbox.com/mapbox-gl-js/v3.5.1/mapbox-gl.css";
+    document.head.appendChild(link);
+
+    mapboxgl.accessToken =
+      process.env.MAPBOX_ACCESS_TOKEN ||
+      "pk.eyJ1IjoiaXNzYWNsMTQiLCJhIjoiY2x5bDlteXEzMWFxazJpcG55N2E5eGt5ZiJ9.ni0ledudRQEsUtuvG_f3zA";
+
+    const map = new mapboxgl.Map({
+      accessToken: mapboxgl.accessToken,
+      container: "map-container",
+      center: [-95.712891, 40.09024],
+      zoom: zoom,
+      style: "mapbox://styles/mapbox/dark-v11",
+      config: {
+        basemap: {
+          lightPreset: "night",
+        },
+      },
+    });
+    return () => {
+      document.head.removeChild(link);
+    };
+  }, [zoom]);
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between bg-yoi-white dark:bg-yoi-black w-screen">
-      <YOINav />
-      <main className="flex-1 z-1">
-        <Banner bg="/shaun-low-v8Un2Roo1Ak-unsplash.jpg" size="small">
-          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl/none fancy">
-            Find A YOI Chapter
-          </h1>
-          <p className="max-w-[600px] text-gray-800 md:text-xl dark:text-gray-400">
-            Use our interactive map to find a YOI chapter near you. If you
-            don&apos;t see a chapter near you, consider starting one!
-          </p>
-        </Banner>
-        <section className="w-full pb-12 md:pb-24 lg:pb-32">
-          {/* add mapkitjs */}
-          <div className="w-full h-96" id="map">
-            <h1>The map is currently unavailable</h1>
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://api.mapbox.com/mapbox-gl-js/v3.5.1/mapbox-gl.css"
+        />
+      </Head>
+      <div className="flex min-h-screen w-screen flex-col items-center justify-between bg-yoi-white dark:bg-yoi-black">
+        <YOINav />
+        <main className="z-1 flex flex-col">
+          <section className="h-screen w-screen pb-12 md:pb-24 lg:pb-32">
+            <div id="map-container" className="h-screen pt-96" />
+          </section>
+          <div className="z-2 bg-radial-ellipse-tl absolute from-yoi-blue-4 from-25% to-transparent pl-6 pr-6 pt-20 dark:from-yoi-blue-1 sm:pt-28">
+            <h1 className="fancy text-4xl sm:text-5xl md:text-6xl lg:text-7xl/none">
+              Find A Location
+            </h1>
+            <p className="max-w-[600px] text-gray-800 dark:text-gray-400 md:text-xl">
+              Use our interactive map to find a location near you. If you
+              don&apos;t see a location near you, consider starting one!
+            </p>
           </div>
-        </section>
-      </main>
-      <YOIFooter />
-    </div>
+        </main>
+        <YOIFooter />
+      </div>
+    </>
   );
 }
