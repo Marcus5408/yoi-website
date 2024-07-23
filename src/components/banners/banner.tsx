e2f8a5eca6b7bfeb95c558238078b3d50f07b3e1
Issac Liu <issac@matchaflavored.site>
2024-07-23 03:05:44 -0400

    refactor: change banner components
    handle overlaying within the component + switch mobile detection method to `useEffect` and `useState`

--- src/components/banners/banner.tsx
+++ src/components/banners/banner.tsx
@@ -1,20 +1,60 @@
-import { useMediaQuery } from "react-responsive";
+import { useEffect, useState } from "react";
 import BannerDesktop from "./banner-desktop";
 import BannerMobile from "./banner-mobile";
 import React from "react";
 
 type BannerProps = {
-  bg?: string;
+  title: any;
+  description: string;
+  bg: string;
   size?: "small" | "medium" | "large";
-  children?: React.ReactNode;
+  buttonName?: string;
+  buttonLink?: string;
 };
 
-const Banner: React.FC<BannerProps> = ({ bg, size, children }) => {
-  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 768px)" });
-  if (isTabletOrMobile) {
-    return <BannerMobile bg={bg ?? ""}>{children}</BannerMobile>;
+const Banner: React.FC<BannerProps> = ({
+  title,
+  description,
+  bg,
+  size,
+  buttonName,
+  buttonLink,
+}) => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  let button = null;
+  if (buttonName !== undefined && buttonLink !== undefined) {
+    button = {
+      text: buttonName,
+      link: buttonLink,
+    };
+  }
+
+  if (isMobile) {
+    return (
+      <BannerMobile
+        bg={bg ?? ""}
+        title={title}
+        description={description}
+        button={button}
+      />
+    );
   } else {
-    return <BannerDesktop bg={bg ?? ""} size={size ?? "large"}>{children}</BannerDesktop>;
+    return (
+      <BannerDesktop
+        bg={bg ?? ""}
+        size={size ?? "large"}
+        title={title}
+        description={description}
+        button={button}
+      />
+    );
   }
 };
 
