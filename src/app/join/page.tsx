c7ba638c273d2cbf537986529907c87d410add23
Issac Liu <issac@matchaflavored.site>
2024-07-23 05:10:22 -0400

    refactor: update images and links on join page

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="flex min-h-screen w-screen flex-col items-center justify-between bg-yoi-white dark:bg-yoi-black">
       <main className="z-1 flex-1">
         <Banner
-          bg="/shaun-low-v8Un2Roo1Ak-unsplash.jpg"
+          bg="/heroes/shaun-low-v8Un2Roo1Ak-unsplash.jpg"
           title="Join Our Mission"
           description="Our mission is to educate the public about ocean threats
             and empower today's youth through advocacy of existing and potential
@@ -17,12 +17,14 @@ export default function Home() {
         />
         <section className="w-full pb-12 md:pb-24 lg:pb-32">
           <TextSection
+            image="/projects/ocg-saving-the-ocean-xch7jXAaqqo-unsplash.jpg"
             title="Find Opportunities"
             description="Look through our curated opportunity database to find an event or opportunity being hosted by us or our partners organizations. We have a variety of opportunities for all ages and skill levels."
-            link="/join/volunteer"
+            link="/resources/opportunities"
             buttonText="Find an Opportunity"
           />
           <TextSection
+            image="/projects/pin-adventure-map-SJvNVEcU5Rc-unsplash.jpg"
             title="Join A Chapter"
             description="Are you a student? Join a chapter at your school or start one! We have chapters throughout the US and are always looking for new members to join our mission."
             link="/join/chapters"
@@ -30,10 +32,11 @@ export default function Home() {
             side="right"
           />
           <TextSection
-            title="Volunteer"
+            image="/projects/windows-p74ndnYWRY4-unsplash.jpg"
+            title="Join the YOI Team"
             description="Join our team of volunteers and help us make a difference. We have a variety of opportunities for all ages and skill levels."
-            link="/join/opportunities?provider=YOI"
-            buttonText="Contact Us"
+            link="/join/yoi"
+            buttonText="Coming Soon"
           />
         </section>
       </main>
