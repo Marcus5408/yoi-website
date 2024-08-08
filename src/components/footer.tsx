afca84ecbb5f6b69d768d27a8e6e5ec4fef49836
Issac Liu <issac@matchaflavored.site>
2024-08-08 06:19:37 -0400

    refactor: update footer text alignment to center

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function YOIFooter() {
   return (
     <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-6 sm:flex-row md:px-6">
-      <p className="text-xs text-gray-500 dark:text-gray-400">
+      <p className="text-xs text-gray-500 dark:text-gray-400 text-center">
         © 2024 The Youth Oceanic Initiative. All rights reserved.
       </p>
       <nav className="flex gap-2 sm:ml-auto sm:gap-6">
