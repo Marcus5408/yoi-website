3f7110784561dc529f68f17b5b43175c6db72397
Marcus Jones <marcusjones8119@gmail.com>
2024-08-08 05:44:23 -0400

    refactor: fix tablet for WOD2024

--- src/app/projects/WOD2024/page.tsx
+++ src/app/projects/WOD2024/page.tsx
@@ -14,10 +14,10 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-screen flex-col items-center justify-between bg-yoi-white dark:bg-yoi-black">
+    <div className="flex w-full flex-col bg-yoi-white dark:bg-yoi-black">
       <main className="z-1 flex-1">
         <Banner
-          bg="/heroes/blue_mountains.png"
+          bg="/heroes/sea mines - Kevin Lin.png"
           size="medium"
           title="World Ocean Day 2024 Zine"
           description="To commemorate World Ocean Day 2024, we organized a 
@@ -25,7 +25,7 @@ export default function Home() {
             or literary creations. A selection of these submissions was then
             curated into a zine."
         />
-        <section className="w-full px-8 py-12 pt-24 sm:px-0 md:py-24 lg:py-32">
+        <section className="w-[100dvw] py-12 pt-24 md:py-32 lg:py-32">
           <h1 className="-mt-12 pb-12 text-center text-3xl font-bold sm:text-4xl xl:text-4xl/none">
             Thank you to all the participants!
           </h1>
@@ -39,60 +39,62 @@ export default function Home() {
             artists that will receive monetary prizes from YOI during the
             webinar, and the winners were...
           </p>
-          <div className="flex flex-col gap-6 px-4 pt-6 sm:flex-row sm:gap-20 sm:px-96 sm:pt-12">
-            <Card className="sm:w-1/2">
-              <CardHeader>
-                <div className="flex items-center justify-center gap-4">
-                  <Avatar className="size-[8rem] content-center">
-                    <AvatarImage
-                      src="/projects/wod2024/kevin.png"
-                      alt="Kevin Lin, Age 17"
-                    />
-                    <AvatarFallback>KL</AvatarFallback>
-                  </Avatar>
-                  <CardTitle className="font-medium">
-                    Kevin Lin, Age 17
-                  </CardTitle>
-                </div>
-              </CardHeader>
-              <CardContent>
-                <CardDescription className="text-center">
-                  Kevin is a rising high school senior from Brooklyn, New York.
-                  He believes ocean preservation and protecting the ocean from
-                  Kevin is a rising high school senior from Brooklyn, New York.
-                  He believes ocean preservation and protecting the ocean from
-                  man-made pollutants is crucial for the health of the ocean as
-                  well as our wellbeing. He fights for the preservation of the
-                  ocean through creating art.
-                </CardDescription>
-              </CardContent>
-            </Card>
-            <Card className="sm:w-1/2">
-              <CardHeader>
-                <div className="flex items-center justify-center gap-4">
-                  <Avatar className="size-[8rem] content-center">
-                    <AvatarImage
-                      src="/projects/wod2024/ava.jpeg"
-                      alt="Ava Saucedo, Age 17"
-                    />
-                    <AvatarFallback>AS</AvatarFallback>
-                  </Avatar>
-                  <CardTitle className="font-medium">
-                    Ava Saucedo, Age 17
-                  </CardTitle>
-                </div>
-              </CardHeader>
-              <CardContent>
-                <CardDescription className="text-center">
-                  Ava is a rising senior at Woodbridge High in Irvine
-                  California. Her chosen medium (photography) aims to convey a
-                  feelings of simplicity and overwhelmed. She believes that the
-                  ocean is grand and untouchable. She hopes her photography is
-                  able to show how this grand beauty interacts with other
-                  natural elements!
-                </CardDescription>
-              </CardContent>
-            </Card>
+          <div className="pt-6 grid grid-rows-1 mx-5 lg:mx-[5em] xl:mx-[10em]">
+            <div className="flex flex-col gap-6 md:table md:table-fixed md:border-spacing-x-8 sm:gap-6">
+              <Card className="md:table-cell md:w-1/3">
+                <CardHeader>
+                  <div className="flex items-center justify-center gap-4">
+                    <Avatar className="size-[8rem] content-center">
+                      <AvatarImage
+                        src="/projects/wod2024/kevin.png"
+                        alt="Kevin Lin, Age 17"
+                      />
+                      <AvatarFallback>KL</AvatarFallback>
+                    </Avatar>
+                    <CardTitle className="font-medium">
+                      Kevin Lin, Age 17
+                    </CardTitle>
+                  </div>
+                </CardHeader>
+                <CardContent>
+                  <CardDescription className="text-center">
+                    Kevin is a rising high school senior from Brooklyn, New
+                    York. He believes ocean preservation and protecting the
+                    ocean from Kevin is a rising high school senior from
+                    Brooklyn, New York. He believes ocean preservation and
+                    protecting the ocean from man-made pollutants is crucial for
+                    the health of the ocean as well as our wellbeing. He fights
+                    for the preservation of the ocean through creating art.
+                  </CardDescription>
+                </CardContent>
+              </Card>
+              <Card className="md:table-cell md:w-1/3">
+                <CardHeader>
+                  <div className="flex items-center justify-center gap-4">
+                    <Avatar className="size-[8rem] content-center">
+                      <AvatarImage
+                        src="/projects/wod2024/ava.jpeg"
+                        alt="Ava Saucedo, Age 17"
+                      />
+                      <AvatarFallback>AS</AvatarFallback>
+                    </Avatar>
+                    <CardTitle className="font-medium">
+                      Ava Saucedo, Age 17
+                    </CardTitle>
+                  </div>
+                </CardHeader>
+                <CardContent>
+                  <CardDescription className="text-center">
+                    Ava is a rising senior at Woodbridge High in Irvine
+                    California. Her chosen medium (photography) aims to convey a
+                    feelings of simplicity and overwhelmed. She believes that
+                    the ocean is grand and untouchable. She hopes her
+                    photography is able to show how this grand beauty interacts
+                    with other natural elements!
+                  </CardDescription>
+                </CardContent>
+              </Card>
+            </div>
           </div>
         </section>
         <section className="w-full py-12 md:py-24 lg:py-32">
