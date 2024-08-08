2172c6556d1e785eb14263b4ea4f8f8924f737af
Marcus Jones <marcusjones8119@gmail.com>
2024-08-08 05:46:04 -0400

    refactor: update team page layout to use full width

--- src/app/about/team/page.tsx
+++ src/app/about/team/page.tsx
@@ -21,7 +21,7 @@ import PersonCard from "@/components/person.tsx";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-screen flex-col items-center justify-between bg-yoi-white dark:bg-yoi-black">
+    <div className="flex min-h-screen w-full flex-col items-center justify-between bg-yoi-white dark:bg-yoi-black">
       <main className="z-1 flex-1">
         <Banner
           bg="/heroes/blue_mountains.png"
