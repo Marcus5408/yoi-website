7f174b0f15471d7bfd93713bfe9fd03c4f0ea800
Issac Liu <issac@matchaflavored.site>
2024-12-03 18:06:02 -0500

    refactor: change navigation to use CSS-based layout selection

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,21 +1,14 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import Navbar from "./navbar";
 import Sidebar from "./sidebar";
 
 function YOINav() {
-  const [isMobile, setIsMobile] = useState(false);
-  useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 910);
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
 
   return (
     <div className="absolute z-10 w-full">
-      {isMobile ? <Sidebar /> : <Navbar />}
+      <Navbar className="hidden lg:flex" />
+      <Sidebar className="block lg:hidden" />
     </div>
   );
 }
